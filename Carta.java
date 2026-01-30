62d7d45af845ac8095f5c88a021652286359ab92
Javier Amaya <javieramayanavarro07@gmail.com>
2026-01-30 10:38:36 +0100

    Implementacion de los contructores Carta

--- Carta.java
+++ Carta.java
@@ -5,6 +5,15 @@ public class Carta {
     public String palo; //(♠ ♥ ♦ ♣)
     public String valor; //(2–10, J, Q, K, A)
 
-    
+    /* Contructores */
+
+    public Carta (String cPalo, String cValor) {
+
+        this.palo = cPalo; //Ponemos el this para referirnos al de arriba
+        this.valor = cValor; 
+    }
+
+    /* Setters y Getters */
+
     
 }
