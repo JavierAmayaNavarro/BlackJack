75ea40ae6c0fe31d755f07831acbed790e2444f0
Javier Amaya <javieramayanavarro07@gmail.com>
2026-02-11 17:30:04 +0100

    prueba

--- Jugador.java
+++ Jugador.java
@@ -1,3 +1,28 @@
 public class Jugador {
+    f
+    gva
+    cfa
+    a
+    sdcv
+    asd
+    as
+    cas
+    dc
+    as
+    dca
+    sd
+    ca
+    sd
+    as
+    dcas
+    dc
+    asd
+    cas
+    cas
+    dc
+    as
+    dc
+    asdc
+    
     
 }
