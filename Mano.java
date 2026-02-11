534e6b31512a512c11a110df44cc47ef3cd69f4a
Pedro <pedropalomadaw@gmail.com>
2026-02-11 18:05:24 +0100

    mano inicio

--- Mano.java
+++ Mano.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class Mano {
-    
+    private ArrayList<Carta> cartas; //Creamos un ArrayList de cartas para representar la mano del jugador
+
+    //Constructor de la clase Mano
+    public Mano() {
+        cartas = new ArrayList<>(); //Inicializamos el ArrayList de cartas
+    }
+
+    public void agregarCarta(Carta carta) {
+        cartas.add(carta); //Agregamos una carta a la mano utilizando el metodo add del ArrayList
+    }
+
+    public int obtenerValorMano() {
+        int valortotal = 0;
+        int numAses = 0;
+
+        //Calculamos el valor total de la mano
+        for(Carta carta : cartas) {
+            valortotal += carta.getValorNumerico(); //Sumamos el valor de la carta a el valor de nuestra mano
+            if(carta.getValor().equals("A")) {
+                numAses++; //Si la carta es un As, incrementamos el contador de Ases
+             }
+
+             while (valortotal > 21 && numAses > 0) //Si el valor total de la mano es mayor a 21 y tenemos ases en la mano, podemos utilizar los ases como valor de 1 en lugar de 11
+             
+            }
+        }
+    }
 }
