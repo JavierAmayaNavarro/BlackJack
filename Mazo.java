e1eee0147b7dd0dc19acbfc49c100de67e819df6
Pedro <pedropalomadaw@gmail.com>
2026-02-11 18:10:25 +0100

    1111

--- Mazo.java
+++ Mazo.java
@@ -1,17 +1,17 @@
 import java.util.Stack;
 import java.util.Collections;
 
-    public class Mazo{
-        private Stack<Carta> cartas; //Creamos una pila de cartas gracias a la clase Stack
+public class Mazo{
+    private Stack<Carta> cartas; //Creamos una pila de cartas gracias a la clase Stack
 
         //Constructor de la clase Mazo
-        public Mazo() {
+    public Mazo() {
             cartas = new Stack<>(); //Inicializamos la pila de carta
             iniciarMazo(); //Llamamos al metodo iniciarMazo para llenar el mazo con las cartas
             barajar(); //Llamamos al metodo barajar para mezclar las cartas del mazo
         }
 
-        private void iniciarMazo() {
+    private void iniciarMazo() {
             String[] palos = {"♠", "♥", "♦", "♣"}; //Creamos un array con los palos de las cartas
             String[] valores = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}; //Creamos un array con los valores de las cartas
 
@@ -21,19 +21,18 @@ import java.util.Collections;
                 }}
         }
 
-        public void barajar() {
+    public void barajar() {
             Collections.shuffle(cartas); //Utilizamos el metodo shuffle de la clase Collections para mezclar las cartas del mazo
         }
 
-        public Carta repartirCarta() {
-            if (!cartas.isEmpty()) { //Esto sirve para comprobar que el mazo no esté vacío antes de repartir una carta
-                return cartas.pop(); //Devuelve la carta que esta primera en el mazo y la elimina del mazo
-            }
-            return null; //Si el mazo esta vacio, devuelve null
+    public Carta repartirCarta() {
+        if (!cartas.isEmpty()) { //Esto sirve para comprobar que el mazo no esté vacío antes de repartir una carta
+            return cartas.pop(); //Devuelve la carta que esta primera en el mazo y la elimina del mazo
+        }
+        return null; //Si el mazo esta vacio, devuelve null
         }
 
-        public boolean esVacio() {
-            return cartas.isEmpty(); //Devuelve true si el mazo esta vacio, de lo contrario devuelve false
+    public boolean esVacio() {
+    return cartas.isEmpty(); //Devuelve true si el mazo esta vacio, de lo contrario devuelve false
         }
     }
-}
