66f07cd02ab4f24e773b45609c7170d59e40da51
Pedro <pedropalomadaw@gmail.com>
2026-02-11 17:34:50 +0100

    inicio mazo

--- Mazo.java
+++ Mazo.java
@@ -1,3 +1,13 @@
 public class Mazo {
-    
+    import java.util.Collections;
+    import java.util.Stack;
+
+    public class Mazo{
+        private Stack<Carta> cartas; //Creamos una pila de cartas gracias a la clase Stack
+
+        //Constructor de la clase Mazo
+        public Mazo() {
+            cartas = new Stack<>(); //Inicializamos la pila de cartas
+        }
+    }
 }
