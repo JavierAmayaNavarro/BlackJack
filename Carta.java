18da4a147239c43f4be3ed51836ec92643c3b0d4
Javier Amaya <javieramayanavarro07@gmail.com>
2026-02-12 17:12:37 +0100

    Mejoras en la clase Carta y Mazo: se añaden atributos y métodos para gestionar el estado de las cartas y optimizar el reparto.

--- Carta.java
+++ Carta.java
@@ -1,9 +1,17 @@
 public class Carta {
 
+    /* Constantes */
+
+    public static final String[] PALOS = {"♠ Picas", "♥ Corazones", "♦ Diamantes", "♣ Tréboles"};
+    public static final String[] VALORES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+
     /*ATRIBUTOS DE LA VARIABLE CARTA */
 
     private String palo; //(♠ ♥ ♦ ♣) //Se pone privado ya que no queremos que nadie de afuera de la clase pueda modificar el palo de la carta, solo queremos que lo obtengan mediante los getters
     private String valor; //(2–10, J, Q, K, A)
+    private int puntos; //Valor numérico de la carta, se asigna al crear la carta a partir del valor de la carta, esto se hace para que no tengamos que calcular el valor numerico cada vez que queramos obtener los puntos de la carta, sino que lo hacemos una sola vez al crear la carta y luego simplemente obtenemos el valor de los puntos
+    private boolean bocaAbajo; //Indica si la carta está boca abajo o no, se inicializa como true ya que al crear una carta, esta estará boca abajo
 
     /* Contructores */
 
@@ -11,6 +19,8 @@ public class Carta {
 
         this.palo = cPalo; //Ponemos el this para referirnos al de arriba
         this.valor = cValor; 
+        this.puntos = getValorNumerico(); //Asignamos el valor numerico de la carta a los puntos, esto se hace para que no tengamos que calcular el valor numerico cada vez que queramos obtener los puntos de la carta, sino que lo hacemos una sola vez al crear la carta y luego simplemente obtenemos el valor de los puntos
+        this.bocaAbajo = false;// Inicializamos la carta boca abajo 
     }
 
     /* Getters */
@@ -23,6 +33,20 @@ public class Carta {
         return this.valor; //Hace referencia al valor de la carta fuera del constructor
     }
 
+    public int getPuntos() {
+        return this.puntos; //Hace referencia a los puntos de la carta fuera del constructor
+    }
+    public boolean isBocaAbajo()  { 
+        return bocaAbajo;  //Hace referencia a si la carta esta boca abajo o no fuera del constructor 
+    }
+    
+    public void setBocaAbajo(boolean bocaAbajo) {
+        this.bocaAbajo = bocaAbajo; 
+    }
+
+    public boolean esAs() {
+         return valor.equals("A"); 
+    }
     
     // Los setters no los vamos a cambiar ya que la carta no va a cambiar su valor ni su palo, solamente la vamos a obtener sin llegar a modificarla 
 
@@ -42,7 +66,8 @@ public class Carta {
         return Integer.parseInt(this.valor); //Convierte el valor de la carta a un numero entero a partir de 2 - 10 
 
     }
-    
+
+       
     @Override
     public String toString() {
         return this.valor + " de " + this.palo; //Devuelve el valor de la carta y su palo
--- Mazo.java
+++ Mazo.java
@@ -2,6 +2,8 @@ import java.util.Stack;
 import java.util.Collections;
 
 public class Mazo{
+
+    /* Atributos de la clase Mazo */
     private Stack<Carta> cartas; //Creamos una pila de cartas gracias a la clase Stack
 
         //Constructor de la clase Mazo
@@ -17,22 +19,36 @@ public class Mazo{
 
             for (String palo : palos) { //Recorremos el array de palos
                 for (String valor : valores) { //Se recorre el array de valores
-                cartas.push(new Carta(palo, valor)); //Agregamos cada carta al mazo utilizando el metodo push de la pila
+                cartas.add(new Carta(palo, valor)); //Agregamos cada carta al mazo utilizando el metodo add de la lista
                 }}
         }
+
+    /* Metodo para Barajar las cartas */
 
     public void barajar() {
             Collections.shuffle(cartas); //Utilizamos el metodo shuffle de la clase Collections para mezclar las cartas del mazo
         }
 
+    /* Metodo para repartir la carta del tope del mazo
+    Si el mazo se agota, se reinicializa y vuelve a barajar automaticamente */
     public Carta repartirCarta() {
         if (!cartas.isEmpty()) { //Esto sirve para comprobar que el mazo no esté vacío antes de repartir una carta
-            return cartas.pop(); //Devuelve la carta que esta primera en el mazo y la elimina del mazo
+            System.out.println("  ⚠ El mazo se ha agotado. Reiniciando y barajando..."); //En caso de que se agote el mazo, muestra que se ha agotado y se reinicia
+            iniciarMazo(); //Llamamos al metodo iniciarMazo para llenar el mazo con las cartas
+            barajar(); //Llamamos al metodo barajar para mezclar las cartas del mazo
+            return cartas.remove(cartas.size() - 1); //Devuelve la carta que esta primera en el mazo y la elimina del mazo
         }
-        return null; //Si el mazo esta vacio, devuelve null
+        return cartas.remove(cartas.size() - 1); //Si el mazo esta vacio, devuelve la última carta del mazo (que ya no existe)
         }
 
-    public boolean esVacio() {
-    return cartas.isEmpty(); //Devuelve true si el mazo esta vacio, de lo contrario devuelve false
-        }
+    /* Devuelve el número de cartas restantes en el mazo. */
+
+    public int cartasRestantes() {
+        return cartas.size(); // Muestra el tamaño de la lista de cartas
     }
+
+    /* Getters */
+
+    public Stack<Carta> getCartas() {
+        return cartas; }
+}
